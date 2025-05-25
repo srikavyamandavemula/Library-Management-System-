5cff89f719ea5c436eb19762858a8bf6fcce558a
srikavyamandavemula <161767205+srikavyamandavemula@users.noreply.github.com>
2025-05-25 13:27:21 +0530

    Create Home.js

---
+++ frontend/frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from '../components/BookList';
+
+function Home() {
+  return (
+    <div>
+      <h1>Library Management System</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default Home;
